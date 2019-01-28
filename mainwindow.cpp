47d672da725e214a3522c5a8842c7048696ffe31
connor41 <primerovmax@gmail.com>
2019-01-28 02:24:15 +0200

    Remove

--- mainwindow.cpp
+++
@@ -1,9 +0,0 @@
-#include "mainwindow.h"
-
-Mainwindow::Mainwindow(QObject *parent) : QObject(parent), lbl("null") {
-  lbl.show();
-}
-
-void Mainwindow::set(QString) {
- // lbl.setText(QString);
-}
